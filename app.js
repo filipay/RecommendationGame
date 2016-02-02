096d02281cf2c88552408a9dbc9de47979938e2b
student <student@students-MacBook-Pro.local>
2016-02-02 23:58:41 +0000

    + Working on pile construction + Added code for notifying players a card was played + Moved some code around

--- app.js
+++ app.js
@@ -8,97 +8,107 @@ var players = [];
 var uri = 'www.omdbapi.com/?i={movieid}&type=movie&plot=short&tomatoes=true';
 
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'sec_user',
-    password: 'titan123',
-    database: 'secure_login'
+  host: 'localhost',
+  user: 'sec_user',
+  password: 'titan123',
+  database: 'secure_login'
 });
 
 connection.connect();
 
 var movies = [];
-connection.query('SELECT * FROM movies', function (err, rows, fields) {
-    if (err) throw err;
+connection.query('SELECT * FROM movies', function(err, rows, fields) {
+  if (err) throw err;
 
-    rows.forEach(function (row) {
-        movies.push(row);
-    });
-    // console.log(movies);
+  rows.forEach(function(row) {
+    movies.push(row);
+  });
+  // console.log(movies);
 });
 
-
-
 exports.initGame = function(s_io, socket) {
-    "use strict";
-    io = s_io;
-    gameSocket = socket;
+  "use strict";
+  io = s_io;
+  gameSocket = socket;
 
-    gameSocket.on('newPlayer', newPlayer);
-    gameSocket.on('assignCard', cardAssigned);
-    gameSocket.on('disconnect', playerDisconnected);
+  gameSocket.on('newPlayer', newPlayer);
+  gameSocket.on('assignCard', cardAssigned);
+  gameSocket.on('disconnect', playerDisconnected);
 
 };
 
 function possibleMovies(player_movies) {
-    var possibleMovies = [];
-    movies.forEach(function (m) {
-        if (!player_movies.some(function (elem) {
-            return elem.movie_id === m.movie_id;
-        })) {
-            possibleMovies.push(m);
-        }
-    });
-    return possibleMovies;
+  var possibleMovies = [];
+  movies.forEach(function(m) {
+    if (!player_movies.some(function(elem) {
+        return elem.movie_id === m.movie_id;
+      })) {
+      possibleMovies.push(m);
+    }
+  });
+  return possibleMovies;
 }
 
 
 function newPlayer(player) {
-    console.log(player.username + " connected");
-    if (players.indexOf(player) < 0) {
-        player.socket = this.id;
-        player.availableMovies = possibleMovies(player.movieList);
-        players.push(player);
-        // console.log(players);
-        io.sockets.emit('playerJoined', players);
-        this.emit('loadAssets', player.availableMovies);
-    } else {
-        console.log("this player already exists");
-    }
+  console.log(player.username + " connected");
+  if (players.indexOf(player) < 0) {
+    player.socket = this.id;
+    player.availableMovies = possibleMovies(player.movieList);
+    players.push(player);
+    // console.log(players);
+    io.sockets.emit('playerJoined', players);
+    this.emit('loadAssets', player.availableMovies);
+  } else {
+    console.log("this player already exists");
+  }
 
 }
 
 function playerDisconnected() {
 
-    var disconnected = this.id;
-    players.forEach(function (player) {
-        if (player.socket === disconnected.id) {
-            disconnected = player;
-        }
-    });
-    console.log(disconnected.username + ' disconnected');
+  var disconnected = this.id;
+  console.log(disconnected);
+  players.forEach(function(player) {
+    if (player.socket === disconnected.id) {
+      disconnected = player;
+    }
+  });
+  console.log(disconnected);
 
-    players.splice(disconnected, 1);
-    io.sockets.emit('playerJoined', players);
-
+  players.splice(disconnected, 1);
+  console.log('player len = ' + players.length);
+  io.sockets.emit('playerJoined', players);
 }
 
 
 function cardAssigned(data) {
-    console.log(data);
-    var assignedPlayer;
+  console.log(data);
+  var assignedPlayer;
 
-    players.forEach(function(player) {
-        if (player.username === data.assignedTo) {
-            assignedPlayer = player;
-        }
-    });
-
-    if (assignedPlayer.movieList.some(function (movie) {
-        return movie.movie_id === data.movie;
-    })) {
-        console.log('MATCH FOUND');
-        this.emit('updateScore', {
-            score: 10
-        });
+  players.forEach(function(player) {
+    if (player.username === data.assignedTo) {
+      assignedPlayer = player;
     }
+  });
+
+  if (assignedPlayer.movieList.some(function(movie) {
+      return movie.movie_id === data.movie;
+    })) {
+    console.log('MATCH FOUND');
+    this.emit('updateScore', {
+      score: 10
+    });
+  }
+  var chosen_movie = getMovie(data.movie);
+  // console.log(chosen_movie);
+  io.sockets.emit('placeOnPile', chosen_movie);
+}
+
+function getMovie(movie_id) {
+  var movie;
+  movies.forEach(function (m) {
+    if (m.movie_id === movie_id) { movie = m; }
+  });
+  return movie;
 }
--- public/js/game.js
+++ public/js/game.js
@@ -3,63 +3,63 @@
  */
 
 var Container = PIXI.Container,
-    TextureCache = PIXI.utils.TextureCache,
-    loader = PIXI.loader,
-    resources = PIXI.loader.resources,
-    Sprite = PIXI.Sprite,
-    Graphics = PIXI.Graphics,
-    Text = PIXI.Text,
-    Point = PIXI.Point;
+  TextureCache = PIXI.utils.TextureCache,
+  loader = PIXI.loader,
+  resources = PIXI.loader.resources,
+  Sprite = PIXI.Sprite,
+  Graphics = PIXI.Graphics,
+  Text = PIXI.Text,
+  Point = PIXI.Point;
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 var currentUser;
 
-
 $.getJSON('/user.php', function(data) {
-    currentUser = new User(data.name, data.username, data.movies);
-    socket.emit('newPlayer', currentUser);
+  currentUser = new User(data.name, data.username, data.movies);
+  socket.emit('newPlayer', currentUser);
 });
 
 var randomMovies = [];
 
 function loadAssets(movies) {
-    randomMovies = movies;
-    movies.forEach(function(movie) {
-        loader.add(movie.poster_url);
-    });
-    loadPlaceholders();
+  randomMovies = movies;
+  movies.forEach(function(movie) {
+    loader.add(movie.poster_url);
+  });
+  loadPlaceholders();
 }
 
 function User(name, username, movieList, avatar) {
-    this.name = name;
-    this.username = username;
-    this.avatar = avatar || 'images/avatar-placeholder.png';
-    this.movieList = movieList;
-    this.score = 0;
+  this.name = name;
+  this.username = username;
+  this.avatar = avatar || 'images/avatar-placeholder.png';
+  this.movieList = movieList;
+  this.score = 0;
 }
 
 var width = 800,
-    height = 600;
+  height = 600;
 
 var renderer = PIXI.autoDetectRenderer(
-    width, height, {
-        antialiasing: true,
-        transparent: false,
-        resolution: window.devicePixelRatio,
-        autoResize: true
-    }
+  width, height, {
+    antialiasing: true,
+    transparent: false,
+    resolution: window.devicePixelRatio,
+    autoResize: true
+  }
 );
 var socket = io.connect('http://localhost:3000');
 socket.on('updateScore', updateScore);
 socket.on('playerJoined', createTable);
 socket.on('loadAssets', loadAssets);
+socket.on('placeOnPile', placeOnPile);
 
 function updateScore(data) {
-    currentUser.score += data.score;
-    currentScore.text = "Your score: " + currentUser.score;
+  currentUser.score += data.score;
+  currentScore.text = "Your score: " + currentUser.score;
 }
 
 renderer.view.style.position = "absolute";
@@ -70,34 +70,34 @@ renderer.backgroundColor = 0x0B5394;
 document.body.appendChild(renderer.view);
 
 function loadPlaceholders() {
-    loader
-        .add("card", "images/card.png")
-        .add("movie", "images/movie-placeholder.jpg")
-        .add("avatar", "images/avatar-placeholder.png")
-        .load(setup);
+  loader
+    .add("card", "images/card.png")
+    .add("movie", "images/movie-placeholder.jpg")
+    .add("avatar", "images/avatar-placeholder.png")
+    .load(setup);
 }
 
 var stage = new Container();
 
 var card_w = 84,
-    card_h = 124;
+  card_h = 124;
 
 
 var currentScore = new PIXI.Text(
-    "Your score : 0", {
-        font: "40px sans-serif",
-        fill: "white"
-    }
+  "Your score : 0", {
+    font: "40px sans-serif",
+    fill: "white"
+  }
 );
 
 currentScore.position.set(50, 50);
 stage.addChild(currentScore);
 
 var waitingText = new PIXI.Text(
-    "Waiting for players...", {
-        font: "40px sans-serif",
-        fill: "white"
-    }
+  "Waiting for players...", {
+    font: "40px sans-serif",
+    fill: "white"
+  }
 );
 waitingText.position.set(window.innerWidth * 0.5, window.innerHeight * 0.5);
 waitingText.anchor.set(0.5, 0.5);
@@ -105,228 +105,265 @@ stage.addChild(waitingText);
 
 function Card(position, movie) {
 
-    var container = new Container();
+  var container = new Container();
 
-    for (var key in movie) {
-        if (movie.hasOwnProperty(key)) {
-            container[key] = movie[key];
-        }
+  for (var key in movie) {
+    if (movie.hasOwnProperty(key)) {
+      container[key] = movie[key];
     }
+  }
 
-    var background = new Sprite(resources.card.texture);
-    background.anchor.set(0.5, 0.5);
-    container.position = position;
+  var background = new Sprite(resources.card.texture);
+  background.anchor.set(0.5, 0.5);
+  container.position = position;
 
-    container.interactive = true;
-    container.buttonMode = true;
+  container.interactive = true;
+  container.buttonMode = true;
 
-    var poster = new Sprite.fromImage(movie.poster_url);
-    poster.width = card_w * 0.7;
-    poster.height = card_h * 0.7;
-    poster.anchor.set(0.5, 0.5);
+  var poster = new Sprite.fromImage(movie.poster_url);
+  poster.width = card_w * 0.7;
+  poster.height = card_h * 0.7;
+  poster.anchor.set(0.5, 0.5);
 
-    var title = new PIXI.Text(movie.title, {
-        font: "10px sans-serif",
-        fill: "black"
+  var title = new PIXI.Text(movie.title, {
+    font: "10px sans-serif",
+    fill: "black"
+  });
+  title.anchor.set(0.5, 0.5);
+  title.position.set(0, -card_h * 0.4);
+
+  container
+  // events for drag start
+    .on('mousedown', onDragStart)
+    .on('touchstart', onDragStart)
+    // events for drag end
+    .on('mouseup', onDragEnd)
+    .on('mouseupoutside', onDragEnd)
+    .on('touchend', onDragEnd)
+    .on('touchendoutside', onDragEnd)
+    // events for drag move
+    .on('mousemove', onDragMove)
+    .on('touchmove', onDragMove)
+    .on('mouseover', function() {
+      this.scale.x = 1.1;
+      this.scale.y = 1.1;
+    })
+    .on('mouseout', function() {
+      this.scale.x = 1;
+      this.scale.y = 1;
     });
-    title.anchor.set(0.5, 0.5);
-    title.position.set(0, -card_h * 0.4);
 
-    container
-    // events for drag start
-        .on('mousedown', onDragStart)
-        .on('touchstart', onDragStart)
-        // events for drag end
-        .on('mouseup', onDragEnd)
-        .on('mouseupoutside', onDragEnd)
-        .on('touchend', onDragEnd)
-        .on('touchendoutside', onDragEnd)
-        // events for drag move
-        .on('mousemove', onDragMove)
-        .on('touchmove', onDragMove)
-        .on('mouseover', function() {
-            this.scale.x = 1.1;
-            this.scale.y = 1.1;
-        })
-        .on('mouseout', function() {
-            this.scale.x = 1;
-            this.scale.y = 1;
-        });
-
-    container.addChild(background);
-    container.addChild(poster);
-    container.addChild(title);
-    return container;
+  container.addChild(background);
+  container.addChild(poster);
+  container.addChild(title);
+  return container;
 }
 
 function Player(position, user) {
-    var player = new Container();
-    player.position = position;
-    player.username = user.username;
+  var player = new Container();
+  player.position = position;
+  player.username = user.username;
 
-    var avatar = new Sprite.fromImage(user.avatar);
-    avatar.anchor.set(0.5, 0.5);
-    avatar.width = avatar.height = window.innerHeight * 0.15;
+  var avatar = new Sprite.fromImage(user.avatar);
+  avatar.anchor.set(0.5, 0.5);
+  avatar.width = avatar.height = window.innerHeight * 0.15;
 
-    var username = new Text(
-        user.name, {
-            font: "20px sans-serif",
-            fill: "white"
-        }
-    );
-    username.anchor.set(0.5, 0.5);
-    username.position.set(0, -avatar.height * 0.7);
+  var username = new Text(
+    user.name, {
+      font: "20px sans-serif",
+      fill: "white"
+    }
+  );
+  username.anchor.set(0.5, 0.5);
+  username.position.set(0, -avatar.height * 0.7);
 
 
-    var circle = new Graphics();
-    circle.lineStyle(0);
-    circle.beginFill(0xFFFF0B, 0.5);
-    circle.drawCircle(position.x, position.y, avatar.width * 0.5);
-    circle.endFill();
+  var circle = new Graphics();
+  circle.lineStyle(0);
+  circle.beginFill(0xFFFF0B, 0.5);
+  circle.drawCircle(position.x, position.y, avatar.width * 0.5);
+  circle.endFill();
 
-    avatar.mask = circle;
+  avatar.mask = circle;
 
-    player.addChild(avatar);
-    player.addChild(username);
+  player.addChild(avatar);
+  player.addChild(username);
 
-    return player;
+  return player;
 
 }
 
 
 
 function createHand(movies, container, rotation) {
-    var hand = new Container();
-    hand.position = new Point(container.width * 0.5, container.height * 1.3);
-    // var start_x = x - movies.length * card_w * 0.5;
+  var hand = new Container();
+  hand.position = new Point(container.width * 0.5, container.height * 1);
+  // var start_x = x - movies.length * card_w * 0.5;
 
-    for (i = 0; i < movies.length; i++) {
-        x = i * (card_w + 10);
+  for (i = 0; i < movies.length; i++) {
+    x = i * (card_w + 10);
 
-        var card = Card(new Point(x, 0), movies[i]);
-        card.childNo = i;
-        hand.addChild(card);
-    }
-    // hand.rotation = -Math.PI/10.0;
-    container.addChildAt(hand, 1);
+    var card = Card(new Point(x, 0), movies[i]);
+    hand.addChild(card);
+  }
+  // hand.rotation = -Math.PI/10.0;
+  container.addChildAt(hand, 1);
 }
 
 
 var table = new Container();
 
 function createTable(players) {
-    waitingText.visible = false;
-    stage.removeChild(table);
-    table = new Container();
-    var angle_step = players.length > 2 ? -Math.PI / (players.length - 2) : 1;
-    var radius = window.innerHeight * 0.5;
-    var count = 0;
-    players.forEach(function(player) {
-        if (player.socket !== socket.id) {
-            var angle = angle_step * count;
-            var x = radius * Math.cos(angle);
-            var y = radius * Math.sin(angle);
+  waitingText.visible = false;
+  stage.removeChild(table);
+  table = new Container();
+  var angle_step = players.length > 2 ? -Math.PI / (players.length - 2) : 1;
+  var radius = window.innerHeight * 0.5;
+  var count = 0;
+  players.forEach(function(player) {
+    if (player.socket !== socket.id) {
+      var angle = angle_step * count;
+      var x = radius * Math.cos(angle);
+      var y = radius * Math.sin(angle);
 
-            x += window.innerWidth * 0.5;
-            y += window.innerHeight - 200;
-            console.log(player);
-            var seat = Player(new PIXI.Point(x, y), new User(player.name, player.username));
-            table.addChild(seat);
+      x += window.innerWidth * 0.5;
+      y += window.innerHeight - 200;
+      console.log(player);
+      var seat = Player(new PIXI.Point(x, y), new User(player.name, player.username));
+      table.addChild(seat);
 
-            count++;
-        }
-    });
-    stage.addChildAt(table, 0);
+      count++;
+    }
+  });
+  stage.addChildAt(table, 0);
 
 }
 
+function createPile(container, size) {
+  var pile = new Container();
+
+  pile.width = size.width;
+  pile.height = size.height;
+
+  pile.position = new Point((container.width - pile.width) * 0.5, (container.height - pile.height) * 0.5);
+
+  container.addChild(pile);
+}
+
+
 function setup() {
-    stage.addChild(waitingText);
-    createHand(randomMovies.slice(0, 4), stage);
-    requestAnimationFrame(animate);
+  stage.addChild(waitingText);
+  createHand(getRandomCardList(randomMovies, 5), stage);
+  createPile(stage, {
+    width: 500,
+    height: 500
+  });
+  requestAnimationFrame(animate);
 
 }
 
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    renderer.render(stage);
+  renderer.render(stage);
 }
 
+function getRandomCardList(movies, size) {
+  console.log('m len : ' + movies.length);
+  console.log('size : ' + size);
+  size = movies.length < size ? movies.length : size;
+  var list = [];
+
+  for (var i = 0; i < size; i++) {
+    // console.log(movies);
+    var index = getRandomInt(0, movies.length - 1);
+    list.push(movies[index]);
+
+    movies.splice(index, 1);
+  }
+  // console.log(list);
+  return list;
+}
+
+
 function onDragStart(event) {
-    // store a reference to the data
-    // the reason for this is because of multitouch
-    // we want to track the movement of this particular touch
-    this.data = event.data;
-    this.alpha = 0.8;
-    this.dragging = true;
-    this.player = undefined;
-    this.origin = new PIXI.Point(this.position.x, this.position.y);
+  // store a reference to the data
+  // the reason for this is because of multitouch
+  // we want to track the movement of this particular touch
+  this.data = event.data;
+  this.alpha = 0.8;
+  this.dragging = true;
+  this.player = undefined;
+  this.origin = new PIXI.Point(this.position.x, this.position.y);
 }
 
 function onDragEnd() {
 
-    if (this.username) {
-        assignCard(this);
-    } else {
-        this.position = this.origin;
-    }
+  if (this.assignedTo) {
+    assignCard(this);
+  } else {
+    this.position = this.origin;
+  }
 
-    console.log(this.origin);
-    this.alpha = 1;
+  console.log(this.origin);
+  this.alpha = 1;
 
-    this.dragging = false;
+  this.dragging = false;
 
-    // set the interaction data to null
-    this.data = null;
+  // set the interaction data to null
+  this.data = null;
 }
 
 
 function onDragMove() {
 
-    if (this.dragging) {
-        var newPosition = this.data.getLocalPosition(this.parent);
+  if (this.dragging) {
+    var newPosition = this.data.getLocalPosition(this.parent);
 
-        this.position.x = newPosition.x;
-        this.position.y = newPosition.y;
-        isCardOnPlayer(this);
-    }
+    this.position.x = newPosition.x;
+    this.position.y = newPosition.y;
+    isCardOnPlayer(this);
+  }
 }
 
 function isCardOnPlayer(card) {
-    table.children.forEach(function(player) {
-        if (player.children[0].containsPoint(card.toGlobal(new Point(0, 0)))) {
-            onPlayerHover(player);
-            card.assignedTo = player.username;
-        } else {
-            onPlayerHoverOut(player);
-            card.assignedTo = undefined;
-        }
-    });
+  table.children.forEach(function(player) {
+    if (player.children[0].containsPoint(card.toGlobal(new Point(0, 0)))) {
+      onPlayerHover(player);
+      card.assignedTo = player.username;
+    } else {
+      onPlayerHoverOut(player);
+      card.assignedTo = undefined;
+    }
+  });
 
 }
 
 function onPlayerHover(player) {
-    player.getChildAt(1).style = {
-        font: "20px sans-serif",
-        fill: "yellow"
-    };
+  player.getChildAt(1).style = {
+    font: "20px sans-serif",
+    fill: "yellow"
+  };
 }
 
 function onPlayerHoverOut(player) {
-    player.getChildAt(1).style = {
-        font: "20px sans-serif",
-        fill: "white"
-    };
+  player.getChildAt(1).style = {
+    font: "20px sans-serif",
+    fill: "white"
+  };
 }
 
 function assignCard(card) {
-    console.log(card);
-    socket.emit('assignCard', {
-        assignedTo: card.username,
-        movie: card.movie_id
-    });
+  console.log(card);
+  socket.emit('assignCard', {
+    assignedTo: card.assignedTo,
+    movie: card.movie_id
+  });
 
-    card.parent.removeChild(card);
+  card.parent.removeChild(card);
 
+}
+
+function placeOnPile(movie) {
+  console.log(movie);
 }
