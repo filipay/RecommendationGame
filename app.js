b835150b7dd2c5738ca0b04998013f9c254536ad
Filip Piskor <filipay@hotmail.com>
2016-03-14 19:35:36 +0000

    is this real life

--- app.js
+++ app.js
@@ -299,7 +299,7 @@ function addMovie(movie) {
     rating: movie.vote_average,
     rating_count: movie.vote_count
   };
-  connection.query('SELECT movie_id FROM user_movies WHERE movie_id = ' + movie.id + 'AND user_id = '+ movie.me.id ,
+  connection.query('SELECT movie_id FROM user_movies WHERE movie_id = ' + movie.id + ' AND user_id = '+ movie.me.id ,
     function(err, rows, fields) {
       if (err) throw err;
       if (rows.length < 1) {
